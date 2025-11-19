778da37d1db9041d6184c81e04463843e20d207f
Arthur Gabriel Oliveira de Macedo <arthurgabrielbr10@gmail.com>
2025-11-19 10:36:35 -0300

    realese 2.1

--- backend/src/models/tokenPayload.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tokenPayload = void 0;
-const zod_1 = require("zod");
-exports.tokenPayload = zod_1.z.object({
-    id: zod_1.z.number(),
-    username: zod_1.z.string().regex(/^[A-Za-zÀ-ÖØ-öø-ÿ0-9_-]+$/)
-});
